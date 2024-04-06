302461dd5b074e9ffa61f992e05695cd0a9f85af
lost <lost>
2024-04-07 05:56:55 +0800

    format

--- test/lib_tesla_test.exs
+++ test/lib_tesla_test.exs
@@ -1,21 +1,23 @@
-
 defmodule LibTeslaTest do
   use ExUnit.Case
 
   defmodule HackernewsClient do
     use Tesla
-    adapter Tesla.Adapter.Hackney
-    plug Tesla.Middleware.Opts, adapter: [proxy: {'127.0.0.1', 55556}]
-    plug Tesla.Middleware.Headers, [
-      {"user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"}
-    ]
-    plug Tesla.Middleware.JSON
-    plug Tesla.Middleware.FormUrlencoded
-    plug Tesla.Middleware.PathParams
-    plug Tesla.Middleware.Logger
-    
+    adapter(Tesla.Adapter.Hackney)
+    plug(Tesla.Middleware.Opts, adapter: [proxy: {~c"127.0.0.1", 55556}])
+
+    plug(Tesla.Middleware.Headers, [
+      {"user-agent",
+       "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"}
+    ])
+
+    plug(Tesla.Middleware.JSON)
+    plug(Tesla.Middleware.FormUrlencoded)
+    plug(Tesla.Middleware.PathParams)
+    plug(Tesla.Middleware.Logger)
+
     def get_topstories do
-      get "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+      get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
     end
   end
 
